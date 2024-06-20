416117c9d18a251f569e0339f83819c87bd025c6
komronbekA <143030949+komronbekA@users.noreply.github.com>
2024-06-20 12:13:22 +0500

    Add files via upload

---
+++ modlips.py
@@ -0,0 +1,8 @@
+# Nad lips
+
+compyuter_model = input("compyuter model: ")
+compyuter_proseser = input("compyuter proseser: ")
+compyuter_memory = input("compyuter memory: ")
+compyuter_screen = input("compyuter screen: ")
+
+print(f"you compyuter advantages  model: {compyuter_model}, prosseser: {compyuter_proseser}, memory: {int(compyuter_memory)}, screen: {float(compyuter_screen)}")
